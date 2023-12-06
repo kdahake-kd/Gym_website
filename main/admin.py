2630b2d2b2c5933782dc41fb4e462a550d854f6b
kiran-kd <kdahake412@gmail.com>
2023-12-06 18:50:21 +0530

    register the classes in admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
+admin.site.register(Service)
+admin.site.register(Post)
+admin.site.register(Product)
+admin.site.register(Order)
