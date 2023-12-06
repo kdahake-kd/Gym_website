383d8fbc28ddd1a7727d8b063ab78d6599c4bb05
kiran-kd <kdahake412@gmail.com>
2023-12-06 18:50:59 +0530

    added the logout url in urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.Home,name="Home"),
     path('signup',views.signup,name="signup"),
     path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
     
     
 ]
